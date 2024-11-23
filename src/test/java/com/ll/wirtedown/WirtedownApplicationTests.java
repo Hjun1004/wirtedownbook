03b0f9bd49abd1ea6156305f1a3d8e18b6a0a0b8
Hjun1004 <hjun1004@naver.com>
2024-11-23 22:54:37 +0900

    세팅

---
+++ src/test/java/com/ll/wirtedown/WirtedownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.wirtedown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WirtedownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
